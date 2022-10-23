6aa336f132627dace8cd314180fbcf7b74fbd932
ApostuDaniel <apostudanbs@gmail.com>
2022-10-23 22:41:54 +0300

    Added MRV

--- Laboratory4/algorithm.py
+++ Laboratory4/algorithm.py
@@ -7,14 +7,17 @@ def is_solution(board):
     return len(board.solution) == board.size
 
 
+# Verificam daca urmatoarea mutare este valida, adica daca putem plasa o piesa la pozitia curenta si daca este o
+# mutare posibila pentru urmatoarea regina dupa plasarea la pozitia curenta
 def is_valid(queen_no, position, board):
     is_available_pos = position in board.variables[queen_no]
 
     if not is_available_pos:
         return False
+    # in last row
     if queen_no + 1 == board.size:
         return is_available_pos
-
+    # check that there are available positions on the next row
     next_variables_copy = board.variables[queen_no + 1].copy()
     for next_position in board.variables[queen_no + 1]:
         if position[0] == next_position[0]:
@@ -31,25 +34,34 @@ def print_solution(board):
     print(board.solution)
 
 
-def place_queen(queen_no, position, board):
+# Plasam regina la tuple-ul position pe board
+def place_queen(position, board):
+    # Plasarea reginei
     board.solution.append(position)
     board.board[position[0], position[1]] = 1
+    board.variables[position[1]].remove(position)
 
+    # Invalidam celelalte pozitii de pe coloana pe care am plasat regina
     for i in range(0, board.size):
         if i == position[0]:
             continue
 
         board.board[i, position[1]] -= 1
+        # Stergem pozitia din lista variables
         if (i, position[1]) in board.variables[position[1]]:
             board.variables[position[1]].remove((i, position[1]))
+
+    # Invalidam celelalte pozitii de pe rindul pe care am plasat regina
     for j in range(position[1], board.size):
         if j == position[1]:
             continue
 
         board.board[position[0], j] -= 1
+        # Stergem pozitia din lista variables
         if (position[0], j) in board.variables[j]:
             board.variables[j].remove((position[0], j))
 
+    # Invalidam pozitiile de pe diagonalele de deasupra pozitiei reginei
     i, j = position
     while i >= 0 and j < board.size:
         if (i, j) != position:
@@ -59,6 +71,7 @@ def place_queen(queen_no, position, board):
         i -= 1
         j += 1
 
+    # Invalidam pozitiile de pe diagonalele de desupt pozitia reginei
     i, j = position
     while i < board.size and j < board.size:
         if (i, j) != position:
@@ -71,10 +84,13 @@ def place_queen(queen_no, position, board):
     return board
 
 
-def remove_queen(queen_no, position, board):
+# Stergem regina de la tuple-ul position pe board
+def remove_queen(position, board):
     board.board[position[0], position[1]] = 0
     board.solution.remove(position)
+    board.variables[position[1]] += [position]
 
+    # Eliberam pozitiile de pe coloana pe care am plasat regina
     for i in range(0, board.size):
         if i == position[0]:
             continue
@@ -82,6 +98,8 @@ def remove_queen(queen_no, position, board):
         board.board[i, position[1]] += 1
         if (i, position[1]) not in board.variables[position[1]] and board.board[i, position[1]] == 0:
             board.variables[position[1]].append((i, position[1]))
+
+    # Eliberam pozitiile de pe rindul pe care am plasat regina
     for j in range(position[1], board.size):
         if j == position[1]:
             continue
@@ -90,6 +108,7 @@ def remove_queen(queen_no, position, board):
         if (position[0], j) not in board.variables[j] and board.board[position[0], j] == 0:
             board.variables[j].append((position[0], j))
 
+    # Eliberam pozitiile de pe diagonala deasupra reginei
     i, j = position
     while i >= 0 and j < board.size:
         if (i, j) != position:
@@ -99,6 +118,7 @@ def remove_queen(queen_no, position, board):
         i -= 1
         j += 1
 
+    # Eliberam pozitiile de pe diagonala de desupt regina
     i, j = position
     while i < board.size and j < board.size:
         if (i, j) != position:
@@ -111,18 +131,25 @@ def remove_queen(queen_no, position, board):
     return board
 
 
+# Find the column with the least amount of free spaces
+def minimum_remaining_values_variable(board):
+    free_columns = [i for i in range(0, board.size) if len(board.variables[i]) > 0]
+    min_column = min(free_columns, key=lambda col: len(board.variables[col]))
+    return min_column
+
+
 def forward_check(board, column):
     global has_solution
-
     for i in range(0, board.size):
         if has_solution:
             return
 
         if is_valid(column, (i, column), board):
-            board = place_queen(column, (i, column), board)
+            board = place_queen((i, column), board)
             if is_solution(board):
                 board.print_solution()
                 has_solution = True
             else:
-                forward_check(board, column + 1)
-            board = remove_queen(column, (i, column), board)
+                next_column = minimum_remaining_values_variable(board)
+                forward_check(board, next_column)
+            board = remove_queen((i, column), board)
--- Laboratory4/board.py
+++ Laboratory4/board.py
@@ -4,7 +4,6 @@ import numpy as np
 class PlayingBoard:
     size = 0
     blocked_positions = []
-    initial_board = np.zeros((size, size), dtype=int)
     board = np.zeros((size, size), dtype=int)
     variables = {}
     solution = []
@@ -13,13 +12,11 @@ class PlayingBoard:
         self.size = size
         self.blocked_positions = blocked_positions
         self.board = np.zeros((size, size), dtype=int)
-        self.initial_board = np.zeros((size, size), dtype=int)
         for position in blocked_positions:
-            self.initial_board[position[0], position[1]] = -1
             self.board[position[0], position[1]] = -1
         for j in range(0, size):
             self.variables[j] = [(i, j) for i in range(0, size) if self.board[i, j] != -1]
 
     def print_solution(self):
-        for position in self.solution:
+        for position in sorted(self.solution, key=lambda pos: pos[1]):
             print(f"Q{position[1]}", position, " ")
