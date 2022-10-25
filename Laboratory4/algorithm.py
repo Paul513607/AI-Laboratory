3150b1def1a09b7155a4f1ac78c3d77a76e5816a
ApostuDaniel <apostudanbs@gmail.com>
2022-10-25 12:21:27 +0300

    Modified is_valid to choose the corect next queen

--- Laboratory4/algorithm.py
+++ Laboratory4/algorithm.py
@@ -7,19 +7,19 @@ def is_solution(board):
     return len(board.solution) == board.size
 
 
-# Verificam daca urmatoarea mutare este valida, adica daca putem plasa o piesa la pozitia curenta si daca este o
-# mutare posibila pentru urmatoarea regina dupa plasarea la pozitia curenta
+# Verify if the current move is valid, and if the next queen to be chosen has available moves
 def is_valid(queen_no, position, board):
     is_available_pos = position in board.variables[queen_no]
 
     if not is_available_pos:
         return False
     # in last row
-    if queen_no + 1 == board.size:
+    if len(board.solution) + 1 == board.size:
         return is_available_pos
     # check that there are available positions on the next row
-    next_variables_copy = board.variables[queen_no + 1].copy()
-    for next_position in board.variables[queen_no + 1]:
+    next_queen = minimum_remaining_values_variable(board)[1]
+    next_variables_copy = board.variables[next_queen].copy()
+    for next_position in board.variables[next_queen]:
         if position[0] == next_position[0]:
             next_variables_copy.remove(next_position)
         elif position[1] == next_position[1]:
@@ -34,34 +34,33 @@ def print_solution(board):
     print(board.solution)
 
 
-# Plasam regina la tuple-ul position pe board
+# Place the queen on the  board based on the position tuple
 def place_queen(position, board):
-    # Plasarea reginei
+    # Queen placing
     board.solution.append(position)
     board.board[position[0], position[1]] = 1
     board.variables[position[1]].remove(position)
 
-    # Invalidam celelalte pozitii de pe coloana pe care am plasat regina
+    # Invalidate the positions on the queens column
     for i in range(0, board.size):
         if i == position[0]:
             continue
 
         board.board[i, position[1]] -= 1
-        # Stergem pozitia din lista variables
+        # Remove from the list of available variables
         if (i, position[1]) in board.variables[position[1]]:
             board.variables[position[1]].remove((i, position[1]))
 
-    # Invalidam celelalte pozitii de pe rindul pe care am plasat regina
+    # Invalidate the position on the same row
     for j in range(position[1], board.size):
         if j == position[1]:
             continue
 
         board.board[position[0], j] -= 1
-        # Stergem pozitia din lista variables
         if (position[0], j) in board.variables[j]:
             board.variables[j].remove((position[0], j))
 
-    # Invalidam pozitiile de pe diagonalele de deasupra pozitiei reginei
+    # Invalidate the positions on the diagonals above the queen
     i, j = position
     while i >= 0 and j < board.size:
         if (i, j) != position:
@@ -71,7 +70,7 @@ def place_queen(position, board):
         i -= 1
         j += 1
 
-    # Invalidam pozitiile de pe diagonalele de desupt pozitia reginei
+    # Invalidate the positions on the diagonals under the queen
     i, j = position
     while i < board.size and j < board.size:
         if (i, j) != position:
@@ -84,13 +83,13 @@ def place_queen(position, board):
     return board
 
 
-# Stergem regina de la tuple-ul position pe board
+# Remove the queen from the board on position
 def remove_queen(position, board):
     board.board[position[0], position[1]] = 0
     board.solution.remove(position)
     board.variables[position[1]] += [position]
 
-    # Eliberam pozitiile de pe coloana pe care am plasat regina
+    # Free the positions on the same column as the queen
     for i in range(0, board.size):
         if i == position[0]:
             continue
@@ -99,7 +98,7 @@ def remove_queen(position, board):
         if (i, position[1]) not in board.variables[position[1]] and board.board[i, position[1]] == 0:
             board.variables[position[1]].append((i, position[1]))
 
-    # Eliberam pozitiile de pe rindul pe care am plasat regina
+    # Free the positions on the same row as the queen
     for j in range(position[1], board.size):
         if j == position[1]:
             continue
@@ -108,7 +107,7 @@ def remove_queen(position, board):
         if (position[0], j) not in board.variables[j] and board.board[position[0], j] == 0:
             board.variables[j].append((position[0], j))
 
-    # Eliberam pozitiile de pe diagonala deasupra reginei
+    # Free the positions on the diagonal above the queen
     i, j = position
     while i >= 0 and j < board.size:
         if (i, j) != position:
@@ -118,7 +117,7 @@ def remove_queen(position, board):
         i -= 1
         j += 1
 
-    # Eliberam pozitiile de pe diagonala de desupt regina
+    # Free the positions on the diagonal under the queen
     i, j = position
     while i < board.size and j < board.size:
         if (i, j) != position:
@@ -134,8 +133,8 @@ def remove_queen(position, board):
 # Find the column with the least amount of free spaces
 def minimum_remaining_values_variable(board):
     free_columns = [i for i in range(0, board.size) if len(board.variables[i]) > 0]
-    min_column = min(free_columns, key=lambda col: len(board.variables[col]))
-    return min_column
+    min_columns = sorted(free_columns, key=lambda col: len(board.variables[col]))
+    return min_columns
 
 
 def forward_check(board, column):
@@ -150,6 +149,6 @@ def forward_check(board, column):
                 board.print_solution()
                 has_solution = True
             else:
-                next_column = minimum_remaining_values_variable(board)
+                next_column = minimum_remaining_values_variable(board)[0]
                 forward_check(board, next_column)
             board = remove_queen((i, column), board)
