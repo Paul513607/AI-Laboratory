1a260c4e75f58c5777c45a7ed381944d9538c7ce
ApostuDaniel <apostudanbs@gmail.com>
2022-11-01 13:06:45 +0200

    Modified gitignore

--- Laboratory6&7/main.py
+++ Laboratory6&7/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import parse
+import utils
+from Network import Network
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    all_data = parse.parse_data(r'resources/iris.data')
+    train_data, test_data = utils.split_data(all_data, 0.2)
+    network = Network([4, 4, 3], 15, 0.5)
+    print(train_data)
+    print(test_data)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
