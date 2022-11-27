2d38c303699d96d7a3926049406e9fbdf62f1bf1
Paul Samson <79131709+Paul513607@users.noreply.github.com>
2022-11-27 12:50:41 +0200

    fix

--- Laboratory9/Algorithm.py
+++ Laboratory9/Algorithm.py
@@ -2,14 +2,15 @@ import numpy as np
 
 
 class QLearning:
+    reward_table: np.ndarray
     q_table: np.ndarray
     start_state: tuple
 
     def __init__(self, start_state: tuple = (3, 0)):
-        self.q_table = np.ones((4, 12), dtype=int) * -1
+        self.reward_table = np.ones((4, 12), dtype=int) * -1
         for i in range(1, 11):
-            self.q_table[3, i] = -100
+            self.reward_table[3, i] = -100
         self.start_state = start_state
 
-    def print_q_table(self):
-        print(self.q_table)
+    def print_reward_table(self):
+        print(self.reward_table)
