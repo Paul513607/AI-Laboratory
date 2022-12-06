e258be925b8cda4e07294093cef5e05a99d2091a
Paul Samson <79131709+Paul513607@users.noreply.github.com>
2022-12-06 13:06:03 +0200

    fix

--- Laboratory10/main.py
+++ Laboratory10/main.py
@@ -1,5 +1,10 @@
 import rdflib
+import pprint
 
 
 if __name__ == '__main__':
-    print("Hello world!")
+    graph = rdflib.Graph()
+    graph.parse(r"./data/food.rdf")
+    print("Length: ", len(graph))
+    for stmt in graph:
+        pprint.pprint(stmt)
