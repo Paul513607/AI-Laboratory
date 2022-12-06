70f27b41e42830c1ef53c3a3de21242b8546c7b2
ApostuDaniel <apostudanbs@gmail.com>
2022-12-06 13:05:07 +0200

    wordnet add

--- Laboratory10/main.py
+++ Laboratory10/main.py
@@ -1,5 +1,11 @@
 import rdflib
+import nltk
+from nltk.corpus import wordnet
 
 
 if __name__ == '__main__':
     print("Hello world!")
+    #nltk.download('wordnet')
+    #nltk.download('omw-1.4')
+    syn_arr = wordnet.synsets('room')
+    print(syn_arr[2].definition())
