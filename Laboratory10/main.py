7da0b3e531267fef2ec6b5897ff298ef85a7f814
ApostuDaniel <apostudanbs@gmail.com>
2022-12-11 17:43:25 +0200

    Attempt at task 2

--- Laboratory10/main.py
+++ Laboratory10/main.py
@@ -8,10 +8,21 @@ if __name__ == '__main__':
     graph = rdflib.Graph()
     graph.parse(r"./data/food.rdf")
     print("Length: ", len(graph))
-    for stmt in graph:
-        pprint.pprint(stmt)
+    for subj, pred, obj in graph:
+        printable_subj = graph.qname(subj) if type(subj) is rdflib.URIRef else subj
+        printable_pred = graph.qname(pred) if type(pred) is rdflib.URIRef else pred
+        printable_obj = graph.qname(obj) if type(obj) is rdflib.URIRef else obj
+        print('{} -- {} -- {}'.format(printable_subj, printable_pred, printable_obj))
+
+    # print(graph.serialize(format='turtle'))
+
+    # graph.print()
+    # for subject, predicate, other in graph.triples((None, None, None)):
+    #     print(subject, '--', predicate, '--', other)
+    # for stmt in graph:
+    #     pprint.pprint(stmt)
 
     # nltk.download('wordnet')
     # nltk.download('omw-1.4')
-    syn_arr = wordnet.synsets('room')
-    print(syn_arr[0].definition())
+    # syn_arr = wordnet.synsets('room')
+    # print(syn_arr[0].definition())
