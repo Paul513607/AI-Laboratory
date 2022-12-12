52d299252d67faa4083a32817460c8f62b463acd
ApostuDaniel <apostudanbs@gmail.com>
2022-12-12 13:52:09 +0200

    Finished task 3

--- Laboratory10/main.py
+++ Laboratory10/main.py
@@ -2,17 +2,33 @@ import rdflib
 import pprint
 import nltk
 from nltk.corpus import wordnet
+import questionaire
 
 
 if __name__ == '__main__':
     graph = rdflib.Graph()
     graph.parse(r"./data/food.rdf")
+    questionaire = questionaire.Questionaire(graph)
     print("Length: ", len(graph))
-    for subj, pred, obj in graph:
+    for subj, pred, obj in graph.triples((None, None, None)):
+        if type(subj) is rdflib.BNode or type(obj) is rdflib.BNode:
+            continue
         printable_subj = graph.qname(subj) if type(subj) is rdflib.URIRef else subj
         printable_pred = graph.qname(pred) if type(pred) is rdflib.URIRef else pred
         printable_obj = graph.qname(obj) if type(obj) is rdflib.URIRef else obj
         print('{} -- {} -- {}'.format(printable_subj, printable_pred, printable_obj))
+
+    while True:
+        question, question_triple = questionaire.generate_question()
+        answer = input(question)
+        correct, found = questionaire.validate_question_answer(question_triple, answer)
+        if correct:
+            print('Correct answer')
+        else:
+            print('Wrong answer, correct is ' + (found if type(found) is not rdflib.URIRef else questionaire.graph.qname(found)))
+        should_terminate = input('Terminate program?(Y/n)')
+        if should_terminate.lower() == 'y':
+            break
 
     # print(graph.serialize(format='turtle'))
 
---
+++ Laboratory10/questionaire.py
@@ -0,0 +1,57 @@
+import random
+
+import rdflib
+
+
+class Questionaire:
+    graph: rdflib.Graph
+    triples: list
+
+    def __init__(self, graph: rdflib.Graph):
+        self.graph = graph
+        self.triples = []
+        for subj, pred, obj in graph.triples((None, None, None)):
+            if type(subj) is rdflib.BNode or type(obj) is rdflib.BNode:
+                continue
+            self.triples.append((subj, pred, obj))
+
+    def generate_question(self):
+        triple_idx = random.randint(0, len(self.triples))
+        question_idx = random.randint(0, 2)
+        subj, pred, obj = self.triples[triple_idx]
+        question = ''
+        question_triple: tuple
+        if question_idx == 0:
+            question = 'Cine este in relatia {} cu {} in partea dreapta: '\
+                .format(self.graph.qname(pred), self.graph.qname(obj) if type(obj) is rdflib.URIRef else obj)
+            question_triple = (None, pred, obj)
+        elif question_idx == 1:
+            question = 'Ce relatie este intre {} si {}: ' \
+                .format(self.graph.qname(subj) if type(subj) is rdflib.URIRef else subj, self.graph.qname(obj) if type(obj) is rdflib.URIRef else obj)
+            question_triple = (subj, None, obj)
+        elif question_idx == 2:
+            question = 'Cine este in relatia {} cu {} in partea stanga: ' \
+                .format(self.graph.qname(pred), self.graph.qname(subj) if type(subj) is rdflib.URIRef else subj)
+            question_triple = (subj, pred, None)
+        return question, question_triple
+
+    def validate_question_answer(self, question_triple, answer):
+        found = None
+        if question_triple[0] is None:
+            found = self.graph.value(predicate=question_triple[1], object=question_triple[2], default=None, any=True)
+        elif question_triple[1] is None:
+            pred_generator = self.graph.predicates(subject=question_triple[0], object=question_triple[2])
+            for pred in pred_generator:
+                found = pred
+                if answer == self.graph.qname(pred):
+                    return True, found
+            return False, found
+
+        elif question_triple[2] is None:
+            found = self.graph.value(subject=question_triple[0], predicate=question_triple[1], default=None, any=True)
+
+        if type(found) is rdflib.URIRef:
+            return answer == self.graph.qname(found), found
+        else:
+            return answer == found, found
+
