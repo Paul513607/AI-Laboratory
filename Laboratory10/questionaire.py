d7958a63c41be83789c313319bddbe84e7662b3f
Paul Samson <79131709+Paul513607@users.noreply.github.com>
2022-12-19 19:13:52 +0200

    synset validation

--- Laboratory10/questionaire.py
+++ Laboratory10/questionaire.py
@@ -30,40 +30,49 @@ class Questionaire:
     def generate_question(self):
         triple_idx = random.randint(0, len(self.triples))
         question_idx = random.randint(0, 2)
+        question_idx = 1
         subj, pred, obj = self.triples[triple_idx]
         question = ''
         question_triple: tuple = (None, None, None)
         if question_idx == 0:
-            pred = self.graph.qname(pred)
-            obj = self.graph.qname(obj) if type(obj) is rdflib.URIRef else obj
-            #pred1 = pred if ":" not in pred else pred.split(":")[1]
-            #obj1 = obj if ":" not in obj else obj.split(":")[1]
-            #pred1 = self.get_a_synonym(pred1)
-            #obj1 = self.get_a_synonym(obj1)
+            pred1 = self.graph.qname(pred)
+            obj1 = self.graph.qname(obj) if type(obj) is rdflib.URIRef else obj
+            pred1 = pred1 if ":" not in pred1 else pred1.split(":")[1]
+            pred1 = pred1 if "#" not in pred1 else pred1.split("#")[1]
+            obj1 = obj1 if ":" not in obj1 else obj1.split(":")[1]
+            pred1 = self.get_a_synonym(pred1)
+            obj1 = self.get_a_synonym(obj1)
             question = 'Cine este in relatia {} cu {} in partea dreapta: ' \
-                .format(pred, obj)
+                .format(pred1, obj1)
             question_triple = (None, pred, obj)
         elif question_idx == 1:
-            subj = self.graph.qname(subj) if type(subj) is rdflib.URIRef else subj
-            obj = self.graph.qname(obj) if type(obj) is rdflib.URIRef else obj
-            #subj1 = subj if ":" not in subj else subj.split(":")[1]
-            #obj1 = obj if ":" not in obj else obj.split(":")[1]
-            #subj1 = self.get_a_synonym(subj1)
-            #obj1 = self.get_a_synonym(obj1)
+            subj1 = self.graph.qname(subj) if type(subj) is rdflib.URIRef else subj
+            obj1 = self.graph.qname(obj) if type(obj) is rdflib.URIRef else obj
+            subj1 = subj1 if ":" not in subj1 else subj1.split(":")[1]
+            obj1 = obj1 if ":" not in obj1 else obj1.split(":")[1]
+            subj1 = self.get_a_synonym(subj1)
+            obj1 = self.get_a_synonym(obj1)
             question = 'Ce relatie este intre {} si {}: ' \
-                .format(subj, obj)
+                .format(subj1, obj1)
             question_triple = (subj, None, obj)
         elif question_idx == 2:
-            subj = self.graph.qname(subj) if type(subj) is rdflib.URIRef else subj
-            pred = self.graph.qname(pred)
-            #subj1 = subj if ":" not in subj else subj.split(":")[1]
-            #pred1 = pred if ":" not in pred else pred.split(":")[1]
-            #subj1 = self.get_a_synonym(subj1)
-            #pred1 = self.get_a_synonym(pred1)
+            subj1 = self.graph.qname(subj) if type(subj) is rdflib.URIRef else subj
+            pred1 = self.graph.qname(pred)
+            subj1 = subj1 if ":" not in subj1 else subj1.split(":")[1]
+            pred1 = pred1 if ":" not in pred1 else pred1.split(":")[1]
+            pred1 = pred1 if "#" not in pred1 else pred1.split("#")[1]
+            subj1 = self.get_a_synonym(subj1)
+            pred1 = self.get_a_synonym(pred1)
             question = 'Cine este in relatia {} cu {} in partea stanga: ' \
-                .format(pred, subj)
+                .format(pred1, subj1)
             question_triple = (subj, pred, None)
         return question, question_triple
+
+    def is_same(self, answer, found):
+        for synset in get_synset(found):
+            if answer.lower() == synset.name().split(".")[0]:
+                return True
+        return False
 
     def validate_question_answer(self, question_triple, answer):
         found = None
@@ -71,27 +80,30 @@ class Questionaire:
         def find_answer(generator):
             wrong_answer = None
             for x in generator:
-                if answer == (self.graph.qname(x) if type(x) is rdflib.URIRef else x):
-                    return x
-                wrong_answer = x
+                y = self.graph.qname(x) if type(x) is rdflib.URIRef else x
+                y = y if ":" not in y else y.split(":")[1]
+                y = y if "#" not in y else y.split("#")[1]
+                y_synset_arr = get_synset(y.lower())
+                for synset in y_synset_arr:
+                    name = synset.name()
+                    print(answer.lower(), ' ---------- ', name.split(".")[0])
+                    if answer.lower() == name.split(".")[0]:
+                        return y
+                wrong_answer = y
             return wrong_answer
 
         if question_triple[0] is None:
             subj_generator = self.graph.subjects(predicate=question_triple[1], object=question_triple[2])
-            for x in subj_generator:
-                print("AAAAAAAA: ", x)
             found = find_answer(subj_generator)
         elif question_triple[1] is None:
             pred_generator = self.graph.predicates(subject=question_triple[0], object=question_triple[2])
             found = find_answer(pred_generator)
             if found is not None:
-                found = found.split(":")[1]
+                found = found if ":" not in found else found.split(":")[1]
         elif question_triple[2] is None:
             found = self.graph.value(subject=question_triple[0], predicate=question_triple[1], default=None, any=True)
 
-        if found is None:
-            found = 'None'
         if type(found) is rdflib.URIRef:
-            return answer == self.graph.qname(found), found
+            return self.is_same(answer, self.graph.qname(found)), found
         else:
-            return answer == found, found
+            return self.is_same(answer, found), found
